78cedebd6713241721e3e30a3094fde633d25bc5
Badarau Marius <badaraumarius1979@gmail.com>
2023-01-12 15:41:02 +0200

    added "Start" button

--- scripts/button.js
+++ scripts/button.js
@@ -1,13 +1,22 @@
-const showHeroesButton = document.querySelector("#button");
+const showHeroesButton = document.querySelector("#show-heroes");
 const title = document.querySelector("#game-name");
 const heroes = document.querySelector("#arena");
+const startFightButton = document.querySelector("#start-fight");
 
 
 
 function displayHeroes() {
     title.classList.add("d-none");
     heroes.classList.add("d-flex");
-    console.log('test');
+    
 }
 
 showHeroesButton.addEventListener("click", displayHeroes);
+
+function hideHeroes() {
+    title.classList.add("d-none");
+    heroes.classList.add("d-none");
+    
+}
+
+startFightButton.addEventListener("click", hideHeroes);
